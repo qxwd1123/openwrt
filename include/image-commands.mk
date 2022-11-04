5c17dbf2c9edb841a908cd4769ca2a01a2f22353
a <a@a>
2022-11-04 18:30:19 +0800

    add zstd images

--- include/image-commands.mk
+++ include/image-commands.mk
@@ -385,6 +385,11 @@ define Build/gzip
 	@mv $@.new $@
 endef
 
+define Build/zstd
+	zstd -T0 -c $@ $(1) > $@.new
+	@mv $@.new $@
+endef
+
 define Build/gzip-filename
 	@mkdir -p $@.tmp
 	@cp $@ $@.tmp/$(word 1,$(1))
